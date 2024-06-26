9321530eabafb82cbb7eb3a1713bad5bb88147e2
Gustavo Caixeta <gstvcaixeta@gmail.com>
2024-06-26 09:01:11 -0300

    remove unecessary modules

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -2,8 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppMaterialModule } from '../shared/app-material/app-material.module';
-import { SharedModule } from '../shared/shared.module';
+
+
 import { ClientesRoutingModule } from './clientes-routing.module';
 import { ClientesListComponent } from './components/clientes-list/clientes-list.component';
 import { ClienteFormComponent } from './containers/cliente-form/cliente-form.component';
@@ -14,15 +14,13 @@ import { ClientesService } from './services/clientes.service';
 
 @NgModule({
     imports: [
-        CommonModule,
-        ClientesRoutingModule,
-        AppMaterialModule,
-        SharedModule,
-        ReactiveFormsModule,
-        ClientesComponent,
-        ClienteFormComponent,
-        ClientesListComponent
-    ],
+    CommonModule,
+    ClientesRoutingModule,
+    ReactiveFormsModule,
+    ClientesComponent,
+    ClienteFormComponent,
+    ClientesListComponent
+],
     providers: [ClientesService]
 })
 export class ClientesModule { }
