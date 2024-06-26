f2599da462b773d76aa8173ef73c6e4c385d6435
Gustavo Caixeta <gstvcaixeta@gmail.com>
2024-06-26 09:04:17 -0300

    remove app.module

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule, MatToolbarModule, NgxMaskDirective, NgxMaskPipe, MatSidenavModule),
+        provideAnimationsAsync(),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.error(err));
